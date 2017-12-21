6a07112b823f681d3197dc04c34d44053df9447b
Anwesh43 <anweshthecool0@gmail.com>
2017-12-21 23:26:42 +0530

    handling tap in bottommenucircle

--- bottommenuview/src/main/java/ui/anwesome/com/bottommenuview/BottomMenuView.kt
+++ bottommenuview/src/main/java/ui/anwesome/com/bottommenuview/BottomMenuView.kt
@@ -24,22 +24,23 @@ class BottomMenuView(ctx:Context):View(ctx) {
             canvas.drawRect(RectF(0f,0f,h-0.9f*h*scale,w),paint)
         }
     }
-    data class BottomMenuCircle(var x:Float,var y:Float,var r:Float) {
-        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+    data class BottomMenuCircle(var x:Float,var h:Float,var r:Float) {
+        fun draw(canvas: Canvas, paint: Paint, scale: Float) {
             canvas.save()
-            canvas.translate(x,y)
+            canvas.translate(x, 0.85f*h - 0.8f*h*scale)
             paint.color = Color.GRAY
-            canvas.drawCircle(0f,0f,r,paint)
-            for(i in 0..1) {
+            canvas.drawCircle(0f, 0f, r, paint)
+            for (i in 0..1) {
                 canvas.save()
-                canvas.rotate(i*90f*(1-scale))
-                paint.strokeWidth = r/5
+                canvas.rotate(i * 90f * (1 - scale))
+                paint.strokeWidth = r / 5
                 paint.color = Color.WHITE
                 paint.strokeCap = Paint.Cap.ROUND
-                canvas.drawLine(-r/2,0f,r/2,0f,paint)
+                canvas.drawLine(-r / 2, 0f, r / 2, 0f, paint)
                 canvas.restore()
             }
             canvas.restore()
         }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y - r && y<=this.y+r
     }
 }
