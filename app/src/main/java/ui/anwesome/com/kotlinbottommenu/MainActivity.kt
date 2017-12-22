06324eaa99269c29c39c8a375264185029bbbccc
Anwesh43 <anweshthecool0@gmail.com>
2017-12-23 00:48:02 +0530

    adding bottommenus in activity

--- app/src/main/java/ui/anwesome/com/kotlinbottommenu/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbottommenu/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinbottommenu
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.bottommenuview.BottomMenuView
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        BottomMenuView.create(this)
+        val view = BottomMenuView.create(this)
+        for(i in 1..10) {
+            val text = "hello world ${i}"
+            view.addMenu(text,{Toast.makeText(this,text,Toast.LENGTH_SHORT).show()})
+        }
+        view.addToParent(this)
     }
 }
