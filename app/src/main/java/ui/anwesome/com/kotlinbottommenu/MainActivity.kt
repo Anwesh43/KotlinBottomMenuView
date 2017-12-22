1d8b0245f457fc7321c935dbbbd2c962fdc2a23a
Anwesh43 <anweshthecool0@gmail.com>
2017-12-22 21:59:10 +0530

    using BottomMenuView in activity

--- app/src/main/java/ui/anwesome/com/kotlinbottommenu/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbottommenu/MainActivity.kt
@@ -2,11 +2,13 @@ package ui.anwesome.com.kotlinbottommenu
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.bottommenuview.BottomMenuView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        BottomMenuView.create(this)
     }
 }
